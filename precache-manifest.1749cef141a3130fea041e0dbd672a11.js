0aa445a6b515ed01877dcd4c101bf2a3140c1771
TomislavBatar <wmbl.info@gmail.com>
2019-06-17 14:19:50 +0200

    Updates

---
+++ precache-manifest.1749cef141a3130fea041e0dbd672a11.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c270022f8cdc161512bdff262963c74",
+    "url": "/sezona/index.html"
+  },
+  {
+    "revision": "7b805d58d884ccf0b56d",
+    "url": "/sezona/static/js/2.b2e3ac06.chunk.js"
+  },
+  {
+    "revision": "761a2c7bfe17d0370e18",
+    "url": "/sezona/static/js/main.0fd5db0a.chunk.js"
+  },
+  {
+    "revision": "d15807dd1474aa8e12b4",
+    "url": "/sezona/static/js/runtime~main.60e9b267.js"
+  }
+]);
