200371c4e8905e8a396084a8c15ce85d1cc93480
TomislavBatar <wmbl.info@gmail.com>
2019-06-17 14:18:47 +0200

    postavljen video

--- src/components/VideoBackground/VideoBackground.jsx
+++ src/components/VideoBackground/VideoBackground.jsx
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
+import Cover from 'react-video-cover';
+
+const style = {
+	width: '100vw',
+	height: '100vh',
+	position: 'fixed',
+	top: 0,
+	left: 0,
+	zIndex: -1
+};
 
 export default class VideoBackground extends Component {
+	state = {
+		resizeNotifier: () => {}
+	};
 	render() {
+		const videoOptions = {
+			src: 'http://omnia-web.com/omniaVideo.mp4',
+			autoPlay: true,
+			muted: true,
+			loop: true
+		};
+
 		return (
-			<div>
-				<br />
+			<div style={style}>
+				<Cover
+					videoOptions={videoOptions}
+					remeasureOnWindowResize
+					getResizeNotifier={(resizeNotifier) => {
+						this.setState({
+							resizeNotifier
+						});
+					}}
+				/>
 			</div>
 		);
 	}
