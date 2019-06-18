1b511b557e3365e8cc4519f2894b5b292a0c06a8
TomislavBatar <wmbl.info@gmail.com>
2019-06-18 11:43:31 +0200

    finalizacija
    ubacen tekst svg kontakt dugme

--- src/components/VideoBackground/VideoBackground.jsx
+++ src/components/VideoBackground/VideoBackground.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import Cover from 'react-video-cover';
+import { Navbar, Nav, Row, Col, Container, Image, Button } from 'react-bootstrap';
 
+import './VideoBackground.css';
+import Logo from './imgVideoBackground/logo-omnia.svg';
 const style = {
 	width: '100vw',
 	height: '100vh',
@@ -23,17 +26,49 @@ export default class VideoBackground extends Component {
 		};
 
 		return (
-			<div style={style}>
-				<Cover
-					videoOptions={videoOptions}
-					remeasureOnWindowResize
-					getResizeNotifier={(resizeNotifier) => {
-						this.setState({
-							resizeNotifier
-						});
-					}}
-				/>
-			</div>
+			<React.Fragment>
+				<Container>
+					<Row className="justify-content-between">
+						<Col className="logo-pozicija">
+							<Image className="kartica-logo" src={Logo} />
+						</Col>
+						<Col className="d-flex justify-content-end">
+							<div className="position-linija">
+								<div className="linija" />
+							</div>
+							<span className="u-izradi-tekst">web stranica u izradi</span>
+						</Col>
+					</Row>
+
+					<Row className="">
+						<Col className="pozicija-glavnog-sadrzaja">
+							<div className="">
+								<h2 className="full-service">Full service marketing agencija</h2>
+								<div className="linija-2" />
+								<h1 className="strategija">
+									Strategija. <br />
+									Kreativnost. <br />
+									Rezultat.
+								</h1>
+								<Button className="kontaktirajte-nas">Kontaktirajte nas</Button>
+							</div>
+						</Col>
+					</Row>
+				</Container>
+				<div className="overlay" />
+
+				<div style={style}>
+					<Cover
+						videoOptions={videoOptions}
+						remeasureOnWindowResize
+						getResizeNotifier={(resizeNotifier) => {
+							this.setState({
+								resizeNotifier
+							});
+						}}
+					/>
+				</div>
+			</React.Fragment>
 		);
 	}
 }
